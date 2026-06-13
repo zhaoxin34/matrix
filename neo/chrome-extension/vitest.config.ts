8851552b528dec871c4a62185bf5deae02ed948f
zhaoxin34 <zhaoxin_er@hotmail.com>
2026-06-13 13:50:56 +0800

    opt: 删除无用的工程

--- neo/chrome-extension/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  test: {
-    environment: 'node',
-    setupFiles: ['./test/setup.ts'],
-    include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
-  },
-})
